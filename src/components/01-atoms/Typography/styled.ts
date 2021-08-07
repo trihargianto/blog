a3a3a00f5552c0e54df7d848a6597998c4b49fd6
Tri Hargianto <trihargianto@gmail.com>
2021-08-07 19:37:08 +0700

    add colors to atoms/typography

--- src/components/01-atoms/Typography/styled.ts
+++ src/components/01-atoms/Typography/styled.ts
@@ -5,9 +5,12 @@ import {
   FONT_WEIGHT,
   LINE_HEIGHT,
 } from "../../00-tokens/typography"
+import * as colors from "../../00-tokens/colors"
 import * as types from "./types"
 
 export const Text = styled.span<types.StyledSpanPropTypes>`
+  color: ${colors.NEUTRAL.N800};
+
   ${props => {
     const { size = "body2", isBold = false } = props
 
